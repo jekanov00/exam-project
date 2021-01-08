0737242fae4222c8276c41859d21a85975ff8f0e
Eugene Samoylenko <evgeniysamoylenko2000@gmail.com>
2021-01-08 02:01:14 +0200

    Add event time changing

--- client/src/components/Events/index.jsx
+++ client/src/components/Events/index.jsx
@@ -1,35 +1,48 @@
 import React, { useEffect, useState } from 'react';
-import { formatDistanceToNowStrict, addDays, parseISO } from 'date-fns';
+import { formatDistanceToNowStrict, addMinutes } from 'date-fns';
 import styles from './events.module.sass';
 
 function Events() {
+  const addRemainingTime = (arr) => {
+    const tempArr = arr;
+    tempArr.forEach((e) => (e.remaining = formatDistanceToNowStrict(new Date(e.end))));
+    return tempArr;
+  }
+
+  const [events, setEvents] = useState(addRemainingTime(JSON.parse(localStorage.getItem('events'))));
+
   useEffect(() => {
     if (!localStorage.getItem('events')) {
       localStorage.setItem('events', JSON.stringify([]));
     }
-  });
 
-  const [events, setEvents] = useState(JSON.parse(localStorage.getItem('events')));
+    let intervalId;
+    for (const e of events) {
+      if(new Date().getTime() < new Date(e.end).getTime() && !intervalId){
+        intervalId = setInterval(()=>setEvents(addRemainingTime(JSON.parse(localStorage.getItem('events')))), 1000);
+      } else if(new Date().getTime() >= new Date(e.end).getTime() && intervalId) {
+        clearInterval(intervalId);
+      }
+    }
 
-  const parseEvents = () => {
-    setEvents(JSON.parse(localStorage.getItem('events')));
-  };
+    return ()=>{
+      clearInterval(intervalId);
+    }
+  }, [events]);
 
   const addEvent = (name, date) => {
     const localEvents = JSON.parse(localStorage.getItem('events'));
-
     localEvents.push({
       name,
       start: new Date(),
       end: date,
     });
+    setEvents(addRemainingTime(localEvents));
     localStorage.setItem('events', JSON.stringify(localEvents));
-
-    parseEvents();
   };
 
   const handleEvent = () => {
-    addEvent('test', addDays(new Date(), 5));
+    addEvent(prompt('Name', '123') || 'test', addMinutes(new Date(), 5));
   };
 
   return (
@@ -38,7 +51,7 @@ function Events() {
         <div className={styles.header}>
           <div className={styles.headingContainer}>
             <h1 className={styles.heading}>Live upcomming checks</h1>
-            <button className={styles.addEvent} onClick={handleEvent}>
+            <button className={styles.addEvent} title={'Add event'} onClick={handleEvent}>
               +
             </button>
           </div>
@@ -48,13 +61,17 @@ function Events() {
           </div>
         </div>
         <div className={styles.eventsContainer}>
-          {events.length ? (
-            events.map((e, index) => (
-              <div className={styles.event} key={index}>
-                <p className={styles.eventName}>{e.name}</p>
-                <p className={styles.remainingTime}>{formatDistanceToNowStrict(parseISO(e.end))}</p>
-              </div>
-            ))
+          {events?.length > 0 ? (
+            events
+              .sort((a, b) => new Date(a.end).getTime() - new Date(b.end).getTime())
+              .map((e, index) => (
+                <div className={styles.event} key={index}>
+                  <p className={styles.eventName}>{e.name}</p>
+                  <p className={styles.remainingTime}>
+                    {new Date().getTime() < new Date(e.end).getTime() ? e.remaining : 'Alert'}
+                  </p>
+                </div>
+              ))
           ) : (
             <p className={styles.noEvents}>No Events</p>
           )}
