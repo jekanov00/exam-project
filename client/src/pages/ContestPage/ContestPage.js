528b16db65f2b4043365015232d3a3b57fb139b4
Eugene Samoylenko <evgeniysamoylenko2000@gmail.com>
2022-10-05 17:45:58 +0100

    Add ability to see offers for moderator

--- client/src/pages/ContestPage/ContestPage.js
+++ client/src/pages/ContestPage/ContestPage.js
@@ -52,11 +52,8 @@ class ContestPage extends React.Component {
 
   setOffersList = () => {
     const array = [];
-    for (let i = 0; i < this.props.contestByIdStore.offers.length; i++) {
-      if (
-        this.props.user.role === CONSTANTS.CUSTOMER &&
-        this.props.contestByIdStore.offers[i].status !== CONSTANTS.OFFER_STATUS_PENDING
-      ) {
+    if (this.props.user.role === CONSTANTS.MODERATOR) {
+      for (let i = 0; i < this.props.contestByIdStore.offers.length; i++) {
         array.push(
           <OfferBox
             data={this.props.contestByIdStore.offers[i]}
@@ -67,6 +64,24 @@ class ContestPage extends React.Component {
             date={new Date()}
           />,
         );
+      }
+    } else {
+      for (let i = 0; i < this.props.contestByIdStore.offers.length; i++) {
+        if (
+          this.props.user.role === CONSTANTS.CUSTOMER &&
+          this.props.contestByIdStore.offers[i].status !== CONSTANTS.OFFER_STATUS_PENDING
+        ) {
+          array.push(
+            <OfferBox
+              data={this.props.contestByIdStore.offers[i]}
+              key={this.props.contestByIdStore.offers[i].id}
+              needButtons={this.needButtons}
+              setOfferStatus={this.setOfferStatus}
+              contestType={this.props.contestByIdStore.contestData.contestType}
+              date={new Date()}
+            />,
+          );
+        }
       }
     }
     return array.length !== 0 ? (
