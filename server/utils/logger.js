e6d8fe1d02049bdcad26c472609a07e53ed5b8bf
Eugene Samoylenko <evgeniysamoylenko2000@gmail.com>
2022-10-31 12:31:35 +0000

    Fix errorLogger error

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -4,33 +4,37 @@ const { LOG_FILE_PATH, FILES_PATH } = require('../constants');
 
 exports.logger = () => {
   try {
-    const fileData = [...JSON.parse(fs.readFileSync(LOG_FILE_PATH, { encoding: 'utf8' }))];
-
-    if (
-      fileData.length > 0 &&
-      !fs.existsSync(
-        `${FILES_PATH}errorsBackup/error-log-${format(
-          new Date().setDate(new Date().getDate() - 1),
-          'y-MM-dd',
-        )}.json`,
-      )
-    ) {
-      const newFileData = [];
-
-      fileData.forEach((e) => {
-        newFileData.push({ message: e.message, code: e.code, time: e.time });
-      });
-
-      fs.writeFileSync(
-        `${FILES_PATH}errorsBackup/error-log-${format(
-          new Date().setDate(new Date().getDate() - 1),
-          'y-MM-dd',
-        )}.json`,
-        JSON.stringify(newFileData),
-        { flag: 'wx' },
-      );
-
+    if (fs.readFileSync(LOG_FILE_PATH, { encoding: 'utf8' }) === '') {
       fs.writeFileSync(LOG_FILE_PATH, JSON.stringify([]));
+    } else {
+      const fileData = JSON.parse(fs.readFileSync(LOG_FILE_PATH, { encoding: 'utf8' }));
+
+      if (
+        fileData.length > 0 &&
+        !fs.existsSync(
+          `${FILES_PATH}errorsBackup/error-log-${format(
+            new Date().setDate(new Date().getDate() - 1),
+            'y-MM-dd',
+          )}.json`,
+        )
+      ) {
+        const newFileData = [];
+
+        fileData.forEach((e) => {
+          newFileData.push({ message: e.message, code: e.code, time: e.time });
+        });
+
+        fs.writeFileSync(
+          `${FILES_PATH}errorsBackup/error-log-${format(
+            new Date().setDate(new Date().getDate() - 1),
+            'y-MM-dd',
+          )}.json`,
+          JSON.stringify(newFileData),
+          { flag: 'wx' },
+        );
+
+        fs.writeFileSync(LOG_FILE_PATH, JSON.stringify([]));
+      }
     }
   } catch (err) {
     throw new Error(err);
